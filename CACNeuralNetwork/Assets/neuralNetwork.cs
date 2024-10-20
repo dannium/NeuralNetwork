b792451c5f632a5a5c28b415e3316de2fc115f35
dannium <dannium161@gmail.com>
2024-10-19 22:21:41 -0400

    nn complete
    tehre is an error tho

--- CACNeuralNetwork/Assets/neuralNetwork.cs
+++ CACNeuralNetwork/Assets/neuralNetwork.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class neuralNetwork : MonoBehaviour
 {
+    System.Random random;
+    float destinationX = 10f;
+    float destinationY = 10f;
     //idk if we need the constructor, so i didnt add it, make sure to comment a bunch
     private int[] layers; //amount of neurons in each layer
     private float[][] neurons; //layer of neuron, specific neuron
@@ -11,6 +15,8 @@ public class neuralNetwork : MonoBehaviour
 
     public int layerAmount; //number of layers in network (probably gonna be 4)
     public int[] neuronAmount; //number of nuerons in each layer
+
+    public float bias; // starting weight for all weights
 
 
     private void initNeurons()
@@ -31,13 +37,49 @@ public class neuralNetwork : MonoBehaviour
             for(int j = 0; j < neuronAmount[i]; j++)
             {
                 weights[i][j] = new float[neuronAmount[i]]; //creates an array of weights based on the amount of neurons the weights come from
+
+                for(int k = 0; k < neuronAmount[i]; k++)
+                {
+                    weights[i][j][k] = (float)random.NextDouble() - 0.5f; //sets each weight
+                }
             }
         }
     }
+
+    private float[] inputs()
+    {
+        float[] array = new float[2]; //makes an array with the length being the amount of inputs (2 rn)
+        array[0] = destinationX - transform.position.x;
+        array[1] = destinationY - transform.position.y;
+        return array;
+    }
+
+    private float[] outputs(float[] inputs) {
+
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            neurons[0][i] = inputs[i];
+        }
+
+        for (int i = 1; i < layers.Length; i++) {
+            for(int j = 0; j < neurons[i].Length; j++)
+            {
+                float value = bias;
+                for(int k = 0;k < neurons[i-1].Length; k++)
+                {
+                    value += weights[i - 1][j][k] * neurons[i - 1][k];
+                }
+                neurons[i][j] = (float)Math.Tanh(value); //sets value between -1 and 1
+            }
+        }
+        return neurons[neurons.Length - 1];
+    }
+
      
     // Start is called before the first frame update
     void Start()
     {
+        random = new System.Random(); // Initialize the random variable
         //initalize neurons and weights
         initNeurons();
         initWeights();
@@ -50,6 +92,6 @@ public class neuralNetwork : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector2(transform.position.x + outputs(inputs())[0], transform.position.y + outputs(inputs())[1])*Time.deltaTime; //changes bots position based on outputs
     }
 }
