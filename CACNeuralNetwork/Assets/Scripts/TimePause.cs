f1ac8b508b0e5800f04c4db506621bc4b1bcc6cc
Dakspieth <jrb.games09@gmail.com>
2024-10-21 19:22:05 -0400

    fe

--- CACNeuralNetwork/Assets/Scripts/TimePause.cs
+++ CACNeuralNetwork/Assets/Scripts/TimePause.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TimePause : MonoBehaviour
 {
     public bool pause = false;
     public TimeSlider TS;
     public TMP_Text buttonTxt;
+    public GameObject pauseMenu;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +23,13 @@ public class TimePause : MonoBehaviour
         {
             Time.timeScale = 0f;
             buttonTxt.text = "Play";
-
+            pauseMenu.SetActive(true);
         }
         else
         {
             Time.timeScale = TS.scale;
             buttonTxt.text = "Pause";
+            pauseMenu.SetActive(false);
         }
 
     }
@@ -34,5 +37,14 @@ public class TimePause : MonoBehaviour
     public void OnClick()
     {
         pause = !pause;
+    }
+
+    public void QuitButton()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+    public void RestartButton()
+    {
+        SceneManager.LoadSceneAsync(1);
     }
 }
